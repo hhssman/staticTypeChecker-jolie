cf6b8f2158e7b8f36e9177d12658e5bb432c03f3
Steven E <stevenhhssman@gmail.com>
2023-10-24 12:41:10 +0200

    Tried to make a jolie paser for strings in code

---
+++ src/test/java/staticTypechecker/Parser.java
@@ -0,0 +1,75 @@
+package staticTypechecker;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+
+import jolie.lang.CodeCheckingException;
+import jolie.lang.parse.ParserException;
+import jolie.lang.parse.SemanticVerifier;
+import jolie.lang.parse.ast.Program;
+import jolie.lang.parse.module.ModuleException;
+import jolie.lang.parse.util.ParsingUtils;
+
+public class Parser {
+    public static void main(String[] args) {
+        String testJolieProgram = """
+                    a = 10
+                    a.x = "h"
+                    a.x.y = true
+                    
+                    b = "h"
+                    b.h = 20L
+            
+                    a = b.h
+            
+                    c = a.x.y
+        """;
+        Program program = simpleParser(testJolieProgram);
+        int test = 0;
+    }
+
+    public static Program parser(String code) {
+        String[] none = {};
+        String[] hardcodedPathToStdLib = {System.getenv("JOLIE_HOME") + "/packages"};
+        SemanticVerifier.Configuration semVerConfig = new SemanticVerifier.Configuration( null );
+		semVerConfig.setCheckForMain( false );
+        Program program = null;
+        try {
+            program = ParsingUtils.parseProgram(
+                    new ByteArrayInputStream(code.getBytes()),
+                    new URI("urn:jolieString"),
+                    null,
+                    none,
+                    hardcodedPathToStdLib,
+                    null,
+                    new HashMap<>(),
+                    semVerConfig,
+                    true
+                );
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ParserException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (CodeCheckingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ModuleException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return program;
+    }
+
+    public static Program simpleParser(String code) {
+        String compCode = "service Main{\nmain{\n" + code + "\n}\n}";
+        return parser(compCode);
+    }
+}
