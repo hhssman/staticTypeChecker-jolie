a025f89630f2dc1966f6f963746859c534f6c4db
Steven <stevenhhssman@gmail.com>
2024-01-30 05:56:05 +0100

    Made type for the empty type

---
+++ src/main/java/staticTypechecker/entities/EmptyType.java
@@ -0,0 +1,59 @@
+package staticTypechecker.entities;
+
+import java.util.IdentityHashMap;
+
+import jolie.lang.parse.context.ParsingContext;
+
+public class EmptyType extends Type{
+
+    @Override
+    public boolean isSubtypeOf(Type other) {
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof EmptyType;
+    }
+
+    @Override
+    public ParsingContext context() {
+        throw new UnsupportedOperationException("Unsupported method");
+    }
+
+    @Override
+    public Type shallowCopy() {
+        return copy();
+    }
+
+    @Override
+    public Type shallowCopyExcept(Path p) {
+        throw new UnsupportedOperationException("Unsupported method");
+    }
+
+    @Override
+    public Type copy() {
+        return new EmptyType();
+    }
+
+    @Override
+    public Type copy(IdentityHashMap<Type, Type> rec) {
+        return copy();
+    }
+
+    @Override
+    public String prettyString() {
+        return "Ø";
+    }
+
+    @Override
+    public String prettyString(int level) {
+        throw new UnsupportedOperationException("Unimplemented method 'prettyString'");
+    }
+
+    @Override
+    public String prettyString(int level, IdentityHashMap<Type, Void> recursive) {
+        throw new UnsupportedOperationException("Unimplemented method 'prettyString'");
+    }
+    
+}
